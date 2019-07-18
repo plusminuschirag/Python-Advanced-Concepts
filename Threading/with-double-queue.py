9fe5d75e3c38d87d83f65d4cbdf769144d329fe5
iamchiragsharma <chiragsharmagtbit@outlook.com>
2019-07-18 18:40:55 +0530

    "double queue done"

--- Threading/with-double-queue.py
+++ Threading/with-double-queue.py
@@ -3,15 +3,51 @@
 #faster than lance armstrong's foundation. So if you believe in good karma don't use print function in that
 #Use a queue and when done print the results
 
-# pip3 install gmaps - This package is needed for this what I am going to do next.
 
 from queue import Queue
 from threading import Thread
+import time
+import requests # Install requests module first.
 
-from gmaps import Geocoding
+THREAD_POOL_SIZE = 4
+pairs = ["XRPBTC","XRPETH","XRPINR","BTCINR","XRPUSDT","ETHUSDT","BTCUSDT","BTCTUSD","BTCUSDC"]
 
-api = Geocoding()
+started = time.time()
+    
+def fetch_pair(pair):
+    url = "https://api.coindcx.com/exchange/v1/trades/" + pair
+    response = requests.get(url)
+    data = response.json()
+    return data['trade_data'][0]
 
-locs = {'Delhi','Kolkata','Gujrat','New York','Dubai','Seoul',''}
+pairQueue = Queue()
+for pair in pairs:
+    pairQueue.put(pair)
+resultQueue = Queue()
 
-THREAD_
+
+def dataFetcher(pairQueue,resultQueue):
+    while not pairQueue.empty():
+        try:
+            pair = pairQueue.get(block=False)
+        except Empty:
+            break
+        else:
+            resultQueue.put(fetch_pair(pair))
+            pairQueue.task_done()
+
+threads = [Thread(target=dataFetcher,args=(pairQueue,resultQueue)) for _ in range(THREAD_POOL_SIZE)]
+
+for thread in threads:
+    thread.start()
+
+pairQueue.join()
+
+while threads:
+    threads.pop().join()
+
+while not resultQueue.empty():
+    print(resultQueue.get())
+
+elapsed = time.time() - started
+print("Time Elapsed: {:.2f}s".format(elapsed))
